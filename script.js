d822ccceba270ce888b9a984fb29a65f5437d607
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-06-04 02:24:05 -0300

    Primer commit del juego Truco

---
+++ script.js
@@ -0,0 +1,180 @@
+class Baraja {
+    constructor() {
+        this.baraja = [];
+        this.iniciarBaraja();
+        this.barajar();
+    }
+
+    iniciarBaraja() {
+        this.baraja = [];
+        const palos = ["Copa", "Basto", "Espada", "Oro"];
+        const numeros = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+
+        for (let i in numeros) {
+            for (let j in palos) {
+                this.baraja.push(numeros[i] + " de " + palos[j]);
+            }
+        }
+    }
+
+    barajar() {
+        let numCartas = this.baraja.length;
+        for (let i = 0; i < numCartas; i++) {
+            let j = Math.floor(Math.random() * numCartas);
+            let temp = this.baraja[i];
+            this.baraja[i] = this.baraja[j];
+            this.baraja[j] = temp;
+        }
+    }
+
+    dar() {
+        return this.baraja.pop();
+    }
+}
+
+class Mano {
+    constructor() {
+        this.mano = [];
+        this.tomarMano();
+        this.manoJugada = [];
+    }
+
+    tomarMano() {
+        for (let i = 0; i < 3; i++) {
+            this.mano.push(barajaJuego.dar());
+        }
+    }
+
+    jugarCarta(carta) {
+        this.manoJugada.push(carta);
+        this.mano = this.mano.filter(e => e !== carta);
+    }
+
+    getMano() {
+        let manoRes = "";
+        let numMano = this.mano.length;
+        for (let i = 0; i < numMano; i++) {
+            manoRes = manoRes + " | " + this.mano[i];
+        }
+        return manoRes;
+    }
+
+}
+
+class Juego {
+    constructor() {
+        this.partida = [["Jugador", 0], ["CPU", 0]];
+        this.jerarquia =
+            [
+                ["1 de Espada", 1],
+                ["1 de Basto", 2],
+                ["7 de Espada", 3],
+                ["7 de Oro", 4],
+                ["3 de Oro", 5], ["3 de Espada", 5], ["3 de Copa", 5], ["3 de Basto", 5],
+                ["2 de Oro", 6], ["2 de Espada", 6], ["2 de Copa", 6], ["2 de Basto", 6],
+                ["1 de Copa", 7], ["1 de Oro", 7],
+                ["12 de Oro", 8], ["12 de Espada", 8], ["12 de Copa", 8], ["12 de Basto", 8],
+                ["11 de Oro", 9], ["11 de Espada", 9], ["11 de Copa", 9], ["11 de Basto", 9],
+                ["10 de Oro", 10], ["10 de Espada", 10], ["10 de Copa", 10], ["10 de Basto", 10],
+                ["7 de Copa", 11], ["7 de Basto", 11],
+                ["6 de Oro", 12], ["6 de Espada", 12], ["6 de Copa", 12], ["6 de Basto", 12],
+                ["5 de Oro", 13], ["5 de Espada", 13], ["5 de Copa", 13], ["5 de Basto", 13],
+                ["4 de Oro", 14], ["4 de Espada", 14], ["4 de Copa", 14], ["4 de Basto", 14],
+            ];
+    }
+
+    jerarquiaCarta(carta) {
+        return this.jerarquia.filter(e => e[0] === carta)[0][1];
+    }
+
+    duelo(cartaCPU, cartaJugador) {
+        let fin = false;
+        let ganador;
+        if (this.jerarquiaCarta(cartaJugador) < this.jerarquiaCarta(cartaCPU)) {
+            alert("Mano para Jugador")
+            this.partida[0][1]++;
+            ganador = "Jugador";
+        }
+        else {
+            alert("Mano para CPU")
+            this.partida[1][1]++;
+            ganador = "CPU";
+        }
+        if (this.partida[0][1] == 2) {
+            alert("Gana Jugador: Jugador " + this.partida[0][1] + " - CPU " + this.partida[1][1])
+            fin = true;
+        }
+        else if (this.partida[1][1] == 2) {
+            alert("Gana CPU: CPU " + this.partida[1][1] + " - Jugador " + this.partida[0][1])
+            fin = true;
+        }
+        return { fin, ganador };
+    }
+
+    jugadorInicia(){
+        if (Math.floor(Math.random() * 2)==1){
+            alert("Inicia Jugador");
+            return "Jugador";
+        }
+        else{
+            alert("Inicia CPU");
+            return "CPU";
+        }
+    }
+}
+
+class CPU {
+    constructor() { }
+
+    responderCarta(cartaJugador) {
+        let numCartas = manoCPU.mano.length;
+        let ganaCPU = false;
+        let cartaCPU = 4;
+        for (let i = 0; i < numCartas; i++) {
+            if (juego.jerarquiaCarta(manoCPU.mano[i]) < juego.jerarquiaCarta(manoJugador.manoJugada[cartaJugador])) {
+                cartaCPU = manoCPU.mano[i];
+                ganaCPU = true;
+                break;
+            }
+        }
+        if (ganaCPU === true) {
+            alert("CPU juega la carta: " + cartaCPU);
+            manoCPU.jugarCarta(cartaCPU);
+        }
+        else if (ganaCPU === false) {
+            cartaCPU = manoCPU.mano.pop();
+            alert("CPU juega la carta: " + cartaCPU);
+            manoCPU.jugarCarta(cartaCPU);
+        }
+    }
+
+    jugarCarta() {
+        let cartaCPU = manoCPU.mano.pop();
+        alert("CPU juega la carta: " + cartaCPU);
+        manoCPU.jugarCarta(cartaCPU);
+    }
+}
+
+const juego = new Juego();
+const barajaJuego = new Baraja();
+const manoJugador = new Mano();
+const manoCPU = new Mano();
+const cpu = new CPU();
+
+let res = { fin: false, ganador: juego.jugadorInicia()};
+
+for (let i = 0; i < 3 && res.fin === false; i++) {
+    if (res.ganador === "Jugador") {
+        manoJugador.jugarCarta(prompt("Tu mano es: " + manoJugador.getMano() + " ¿Que carta quieres jugar?"));
+        cpu.responderCarta(i);
+    }
+    else {
+        cpu.jugarCarta();
+        manoJugador.jugarCarta(prompt("Tu mano es: " + manoJugador.getMano() + " ¿Que carta quieres jugar?"));
+    }
+
+    res = juego.duelo(manoCPU.manoJugada[i], manoJugador.manoJugada[i]);
+}
+
+
+
