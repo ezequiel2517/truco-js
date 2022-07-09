89ee3fdb853cc1c181fb2ea887be849e35ef8e42
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-07-09 19:13:15 -0300

    Agregando fetch.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 let juego = new Juego();
+
+async function cargarJerarquia(){
+    const res = await fetch("js/jerarquia.json");
+    juego.jerarquia = await res.json();
+}
+cargarJerarquia();
+
 const interfaz = new Interfaz();
 let barajaJuego = new Baraja();
 let manoJugador = new Mano("Jugador");
