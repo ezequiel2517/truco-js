3468edcbaf25dd6a4ea2d8efd3bc44b09a6441f4
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-07-09 18:50:53 -0300

    Quitando fetch

--- js/main.js
+++ js/main.js
@@ -4,25 +4,3 @@ let barajaJuego = new Baraja();
 let manoJugador = new Mano("Jugador");
 let manoCPU = new Mano("CPU");
 let cpu = new CPU();
-
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//         method: 'POST',
-//         headers:  {
-//             'Content-type': 'application/json; charset=UTF-8',
-//         },
-//         body: JSON.stringify({
-//             title: 'Coderhouse',
-//             body: 'Clase 15',
-//             userId: 5,
-//         })
-//     })
-//     .then((resp) => resp.json())
-//     .then((data) => {
-//         console.log(data)
-//     })
-
-fetch('js/jararquia.json')
-    .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data)
-    })
