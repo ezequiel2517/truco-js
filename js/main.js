78593ae970d3236b1335e9a7f6d50db0d7a3e186
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-07-11 19:54:20 -0300

    Quitando fetch de archivo local.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 let juego = new Juego();
-
-async function cargarJerarquia() {
-    const res = await fetch("js/jerarquia.json");
-    juego.jerarquia = await res.json();
-}
-cargarJerarquia();
-
 const interfaz = new Interfaz();
 let barajaJuego = new Baraja();
 let manoJugador = new Mano("Jugador");
 let manoCPU = new Mano("CPU");
 let cpu = new CPU();
-
-// ajax({
-//     url: 'https://randomuser.me/api/',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
